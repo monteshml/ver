8f4b4272bbc7e5ad0e4912e567093fc90395b72e
monteshml <hernan@debian>
2017-03-16 11:55:48 +0100

    Primera configuracion de django y ver pagina inicial

--- proyecto1/settings.py
+++ proyecto1/settings.py
@@ -81,7 +81,6 @@ DATABASES = {
     }
 }
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
@@ -100,3 +99,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
