34aaa94b3fc9554b5d4bbaedf977eb6b68f14be5
monteshml <hernan@debian>
2017-03-16 10:34:16 +0100

    Cambio Zona horaria e idioma

--- proyecto1/settings.py
+++ proyecto1/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ec'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
